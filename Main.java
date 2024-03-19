a064878254f125372cbb118d5512fbcd548dacb1
CatalinAndrei2003 <162283731+CatalinAndrei2003@users.noreply.github.com>
2024-03-19 09:51:09 +0200

    Add files via upload
    Lab3

--- Main.java
+++ Main.java
@@ -1,31 +1,38 @@
-
 class Form{
 
-  private String color;
-  public Form()
-  {
-      this.color = "white";
-  }
+    private String color;
+    private static int counter = 0;
+    public Form()
+    {
+        this.color = "white";
+        counter++;
+    }
 
-  public Form(String color)
-  {
-      this.color=color;
-  }
+    public Form(String color)
+    {
+        this.color=color;
+        counter++;
+    }
 
-  public float GetArea()
-  {
-      return 0;
-  }
+    public float GetArea()
+    {
+        return 0;
+    }
 
-@Override
-  public String toString()
-  {
-      return "This form has the color " + color;
-  }
+    @Override
+    public String toString()
+    {
+        return "This form has the color " + color;
+    }
+
+    public static int getCounter()
+    {
+        return counter;
+    }
 }
 
 class Triangle extends Form{
-   private float height;
+    private float height;
     private float base;
 
     public Triangle()
@@ -44,7 +51,7 @@ class Triangle extends Form{
 
     public float getArea()
     {
-       return (base+height)/2;
+        return (base+height)/2;
     }
 
     public String toString()
@@ -108,6 +115,60 @@ class Square extends Form{
     }
 }
 
+class StringRandomizer {
+    java.util.Random rand = new java.util.Random();
+
+    public String randomString(int n) {
+        int countLetters = 'z' - 'a';
+        char result[] = new char[n];
+
+        for (int i = 0; i < n; i++) {
+            result[i] = (char) ('a' + rand.nextInt(countLetters));
+        }
+        return new String(result);
+    }
+
+    public String randomString(int n, String alphabet) {
+        int countLetters = alphabet.length();
+        char result[] = new char[n];
+        for (int i = 0; i < n; i++) {
+            result[i] = alphabet.charAt(rand.nextInt(countLetters));
+        }
+
+        return new String(result);
+    }
+}
+
+class PasswordMaker {
+    private static final int MAGIC_NUMBER = 8;
+    private static final String MAGIC_STRING = "abcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()";
+
+    private String name;
+
+    public PasswordMaker(String name) {
+        this.name = name;
+    }
+
+    public String getPassword()
+    {
+        StringRandomizer randomizer = new StringRandomizer();
+
+        String randomPart = randomizer.randomString(MAGIC_NUMBER);
+
+        String alphabet = MAGIC_STRING.substring(5, 15);
+
+        String alphabetPart = randomizer.randomString(10, alphabet);
+
+        String nameLengthPart = String.valueOf(name.length());
+
+        int randomNumber = randomizer.rand.nextInt(51);
+
+        String password = randomPart + alphabetPart + nameLengthPart + randomNumber;
+
+        return password;
+    }
+}
+
 public class Main
 {public static void main(String[] args)
 {
@@ -117,7 +178,9 @@ public class Main
     System.out.println("Triangle area = " + tri.getArea()+" details: " + tri);
     System.out.println("Circle area = " + c.getArea() + " details: " + c);
     System.out.println("Square area = " + sq.getArea() + " details: " + sq);
+    System.out.println("Counter Value = " + Form.getCounter());
+    PasswordMaker passwordMaker = new PasswordMaker("JohnDoe");
+    String password = passwordMaker.getPassword();
+    System.out.println("Generated Password: " + password);
 
 }}
-
-
