4db6eef83636a9bf246e565529c2c9bdd1c77ab0
CatalinAndrei2003 <162283731+CatalinAndrei2003@users.noreply.github.com>
2024-05-28 09:24:37 +0300

    Lab11

--- Main.java
+++ Main.java
@@ -1,14 +1,8 @@
+package org.example;
+
 public class Main {
     public static void main(String[] args) {
-        String file_in = "input_tema.txt";
-        String file_out = "output_tema.txt";
-        PhoneBook phoneBook = new PhoneBook();
-        try {
-            phoneBook.readAndSave(file_in, file_out);
-        }catch (PhoneAppException ex) {
-            ex.printStackTrace();
+
+
         }
-        phoneBook.print(System.out);
-        System.out.println("DONE.");
     }
-}
